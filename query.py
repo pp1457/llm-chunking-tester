e33036d30a4ab6c0dbdc04a9cded66ce122666e7
Paul Liao <liaoyunyang0815@gmail.com>
2024-08-12 15:18:11 +0800

    add query.py

---
+++ query.py
@@ -0,0 +1,55 @@
+import argparse
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_community.llms.ollama import Ollama
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+
+回答以下的問題
+---
+{question}
+--- 
+參考以下內容資料來幫助回答上面的問題
+--- 
+{context}
+---
+"""
+
+def get_embedding_function():
+    embeddings = OllamaEmbeddings(model="shaw/dmeta-embedding-zh")
+    return embeddings
+
+def main():
+
+    query_text = input("Query: ")
+    query_rag(query_text)
+
+def query_rag(query_text: str):
+    embedding_function = get_embedding_function()
+    db = Chroma(
+      persist_directory=CHROMA_PATH,
+      embedding_function=embedding_function
+    )
+  
+    results = db.similarity_search_with_score(query_text, k=5)
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+  
+    # print(prompt)
+  
+    model = Ollama(model="gemma2:9b")
+    response_text = model.invoke(prompt)
+  
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"\n\nResponse:\n {response_text}\nSources:\n {sources}"
+    print(formatted_response)
+    return response_text
+
+if __name__ == "__main__":
+  main()
+
+
